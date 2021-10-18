ec827ee36671f5b5667a5c3f4940f192d739643d
jormazlop <jormazlop@gmail.com>
2021-10-18 23:40:40 +0200

    Entregable Modulo 2

---
+++ Modulo 2/Ejercicio 1/app.js
@@ -0,0 +1,26 @@
+const head = array => {
+
+   const [firstElement,...restArray] = array
+
+   return firstElement;
+}
+
+const tail = ([first,...array]) => {
+   return array;
+}
+
+const init = array => {
+   return array.slice(0, -1);
+}
+
+const last = array => {
+   return array.slice(-1);
+}
+
+const array = ['Primer elemento', 'Segundo elemento', 'Tercer elemento', 'Cuarto elemento'];
+
+
+console.log(head(array));
+console.log(tail(array));
+console.log(init(array));
+console.log(last(array));
